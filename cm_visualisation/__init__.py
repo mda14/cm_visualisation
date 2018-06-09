527879bd4a9bec3d1966d29efc4779c2a3f2998f
mda14 <Chispi@air-de-rocio.home>
2018-06-09 11:30:53 +0100

    better figure resolution

--- cm_visualisation/__init__.py
+++ cm_visualisation/__init__.py
@@ -47,4 +47,4 @@ def cm_analysis(y_true, y_pred, filename, labels, ymap=None, figsize=(10,10)):
     cm.columns.name = 'Predicted'
     fig, ax = plt.subplots(figsize=figsize)
     sns.heatmap(cm, annot=annot, fmt='', ax=ax)
-    plt.savefig(filename)
+    plt.savefig(filename, dpi=280)
